02a5c861b1dddf073efaf2a26522f863fe3d9d7f
deepak76901 <deepakb76901@gmail.com>
2024-05-22 00:02:18 +0530

    bg-color fix

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -6,7 +6,7 @@ export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row border-2 border-teal-500 p-4 rounded-tl-3xl rounded-tr-none rounded-bl-none rounded-br-3xl justify-center items-center">
       <div className="flex flex-col flex-1 p-3">
-        <h2 className="text-2xl">Want to learn more about Cricket?</h2>
+        <h2 className="text-2xl pl-2">Want to learn more about Cricket?</h2>
         <p className="text-gray-500 m-2">Checkout these resources with 100 JavaScript projects</p>
         <Button gradientDuoTone="purpleToPink" className="rounded-bl-none ">
           <a
